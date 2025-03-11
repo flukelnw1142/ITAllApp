cca855dbb92ab16b5886041b52e782f43103db4e
Noppon Eamkham <sengchip1@gmail.com>
2025-03-11 15:32:38 +0700

    Add getThemeSettings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { DashboardService } from './modules/dashboard/service/dashboard.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,15 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = false;
+
+  constructor(private dashboardService: DashboardService, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.dashboardService.getThemeSettings().subscribe(theme => {
+      this.renderer.setStyle(document.documentElement, '--theme-bg-color', theme.BackgroundColor);
+      this.renderer.setStyle(document.documentElement, '--theme-container-bg', theme.ContainerBackgroundColor);
+    });
+  }
 }
