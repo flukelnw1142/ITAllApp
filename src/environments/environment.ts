5890e22dc31e50a480e7f8e728dbd9590ed32e0f
Flukelnw <flukelnw1142@gmail.com>
2025-03-11 14:43:26 +0700

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    api_url: 'http://10.10.0.28:9090/api',
+    api_url: 'http://10.10.0.28:5122/api',
     name: 'environment',
     version: '0.0.0',
     code: 1000,
-    background: ''
+    background: '#3A3A3A'
   };
