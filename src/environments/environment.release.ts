15f52cc027d1189c42bb82745e565e05f906568f
Flukelnw <flukelnw1142@gmail.com>
2025-03-11 15:32:26 +0700

    add branch release

---
+++ src/environments/environment.release.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  api_url: 'http://10.10.0.28:5121/api',
+  name: 'release environment',
+  version: '0.0.0',
+  code: 3000,
+  background: '#3A3A3A'
+};
